5d23f09c4a8d7c13b5888ed12c9e11ff2865387b
Phan Đức Nghĩa <123baconsau6969@gmail.com>
2024-05-29 18:16:16 +0000

    add 3 model

--- server.py
+++ server.py
@@ -3,14 +3,20 @@ from dotenv import load_dotenv
 import google.generativeai as genai
 import os
 import torch
-import transformers
-from transformers import AutoModelForCausalLM, AutoTokenizer
-from transformers import pipeline
+from IPython.display import Markdown
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 
 app = FastAPI()
 
 load_dotenv()
 
+model_phi_id = 'microsoft/phi-2'
+model_phi = AutoModelForCausalLM.from_pretrained(model_phi_id)
+tokenizer_phi = AutoTokenizer.from_pretrained(model_phi_id)
+
+model_tinyllama_id = 'TinyLlama/TinyLlama-1.1B-Chat-v1.0'
+model_tiny_llama = AutoModelForCausalLM.from_pretrained(model_tinyllama_id)
+tokenizer_tiny_llama = AutoTokenizer.from_pretrained(model_tinyllama_id)
 
 @app.get("/")
 async def root():
@@ -51,8 +57,59 @@ async def tinyllama(request: Request):
         data = await request.json()
         message = data.get("message")
         if message is None:
-            pass
+            raise ValueError("Message field is missing")
+        else:
+            pipe = pipeline("text-generation",
+                            model=model_tiny_llama,
+                            tokenizer=tokenizer_tiny_llama,
+                            torch_dtype=torch.bfloat16,
+                            device_map="auto")
+
+            messages = [
+                {"role": "user", "content": f"{message}"},
+            ]
+
+            prompt = pipe.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
+            outputs = pipe(prompt, max_new_tokens=300,
+                           do_sample=True,
+                           temperature=0.7,
+                           top_k=50,
+                           top_p=0.95)
+            results_text = Markdown(outputs[0]["generated_text"])
+            return {"message": results_text}
+            
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
+
+# @app.post('/Phi2/check')
+# async def phi2(request: Request):
+#     try:
+#         data = await request.json()
+#         message = data.get("message")
+#         if message is None:
+#             raise ValueError("Message field is missing")
+#         else:
+#             pipe = pipeline("text-generation",
+#                             model=model_phi,
+#                             tokenizer=tokenizer_phi,
+#                             trust_remote_code=True,
+#                             device_map="auto")
+#
+#             outputs = pipe(
+#                 message,
+#                 max_new_tokens=300,
+#                 do_sample=True,
+#                 temperature=0.7,
+#                 top_k=50,
+#                 top_p=0.95,
+#             )
+#             results_text = Markdown(outputs[0]["generated_text"])
+#             return {"message": results_text}
+#
+#     except ValueError as e:
+#         raise HTTPException(status_code=400, detail=str(e))
+#     except Exception as e:
+#         raise HTTPException(status_code=500, detail=str(e))
