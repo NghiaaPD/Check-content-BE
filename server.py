a50a604fa40013541e0b8d8dc83efbbff12f8a40
Phan Đức Nghĩa <123baconsau6969@gmail.com>
2024-05-30 14:48:20 +0000

    add final code not connect db

--- server.py
+++ server.py
@@ -2,21 +2,25 @@ from fastapi import FastAPI, HTTPException, Request
 from dotenv import load_dotenv
 import google.generativeai as genai
 import os
-import torch
-from IPython.display import Markdown
-from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+# import torch
+# from IPython.display import Markdown
+# from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 
 app = FastAPI()
 
 load_dotenv()
 
-model_phi_id = 'microsoft/phi-2'
-model_phi = AutoModelForCausalLM.from_pretrained(model_phi_id)
-tokenizer_phi = AutoTokenizer.from_pretrained(model_phi_id)
 
-model_tinyllama_id = 'TinyLlama/TinyLlama-1.1B-Chat-v1.0'
-model_tiny_llama = AutoModelForCausalLM.from_pretrained(model_tinyllama_id)
-tokenizer_tiny_llama = AutoTokenizer.from_pretrained(model_tinyllama_id)
+
+# model_phi_id = 'microsoft/phi-2'
+# model_phi = AutoModelForCausalLM.from_pretrained(model_phi_id)
+# tokenizer_phi = AutoTokenizer.from_pretrained(model_phi_id)
+#
+# model_tinyllama_id = 'TinyLlama/TinyLlama-1.1B-Chat-v1.0'
+# model_tiny_llama = AutoModelForCausalLM.from_pretrained(model_tinyllama_id)
+# tokenizer_tiny_llama = AutoTokenizer.from_pretrained(model_tinyllama_id)
 
 @app.get("/")
 async def root():
@@ -51,37 +55,37 @@ async def gemini(request: Request):
         raise HTTPException(status_code=500, detail=f"Internal Server Error: {e}")
 
 
-@app.post('/TinyLlama/check')
-async def tinyllama(request: Request):
-    try:
-        data = await request.json()
-        message = data.get("message")
-        if message is None:
-            raise ValueError("Message field is missing")
-        else:
-            pipe = pipeline("text-generation",
-                            model=model_tiny_llama,
-                            tokenizer=tokenizer_tiny_llama,
-                            torch_dtype=torch.bfloat16,
-                            device_map="auto")
-
-            messages = [
-                {"role": "user", "content": f"{message}"},
-            ]
-
-            prompt = pipe.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
-            outputs = pipe(prompt, max_new_tokens=300,
-                           do_sample=True,
-                           temperature=0.7,
-                           top_k=50,
-                           top_p=0.95)
-            results_text = Markdown(outputs[0]["generated_text"])
-            return {"message": results_text}
-            
-    except ValueError as e:
-        raise HTTPException(status_code=400, detail=str(e))
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+# @app.post('/TinyLlama/check')
+# async def tinyllama(request: Request):
+#     try:
+#         data = await request.json()
+#         message = data.get("message")
+#         if message is None:
+#             raise ValueError("Message field is missing")
+#         else:
+#             pipe = pipeline("text-generation",
+#                             model=model_tiny_llama,
+#                             tokenizer=tokenizer_tiny_llama,
+#                             torch_dtype=torch.bfloat16,
+#                             device_map="auto")
+#
+#             messages = [
+#                 {"role": "user", "content": f"{message}"},
+#             ]
+#
+#             prompt = pipe.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
+#             outputs = pipe(prompt, max_new_tokens=300,
+#                            do_sample=True,
+#                            temperature=0.7,
+#                            top_k=50,
+#                            top_p=0.95)
+#             results_text = Markdown(outputs[0]["generated_text"])
+#             return {"message": results_text}
+#
+#     except ValueError as e:
+#         raise HTTPException(status_code=400, detail=str(e))
+#     except Exception as e:
+#         raise HTTPException(status_code=500, detail=str(e))
 
 
 # @app.post('/Phi2/check')
--- test.py
+++ test.py
@@ -1,6 +1,14 @@
-from dotenv import load_dotenv
-import os
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 
-load_dotenv()
+uri = "mongodb+srv://NghiaPD:598247002584@binacebotnotidb.zijh7pl.mongodb.net/?retryWrites=true&w=majority&appName=BinaceBotNotiDB"
 
-print(os.environ['GEMINI_API_KEY'])
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
